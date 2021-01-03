f04dece31d7e87d9d0288eea4e7b610c28f7c0e3
Marco <masanator@hotmail.de>
2021-01-03 14:45:28 +0100

    fixed rebuild script

--- scripts/clean_and_rebuild.sh
+++ scripts/clean_and_rebuild.sh
@@ -5,6 +5,9 @@
 # Further regulations can be found in LICENSE file.           #
 ###############################################################
 
-catkin clean evo_dcdc_shield_interface evo_logger evo_rd_platform_example evo_tof_board_interface evo_drives_control evo_motor_shield_interface evo_robot_base_interface 
+catkin clean evo_dcdc_shield_interface evo_logger evo_rd_platform_example evo_tof_board_interface evo_motor_shield_interface evo_robot_base_interface 
 
-catkin build evo_dcdc_shield_interface evo_logger evo_rd_platform_example evo_tof_board_interface evo_drives_control evo_motor_shield_interface evo_robot_base_interface 
+catkin build evo_dcdc_shield_interface evo_logger evo_rd_platform_example evo_tof_board_interface evo_motor_shield_interface evo_robot_base_interface 
+
+# rebuild bc of custom msgs and stupid ros..
+catkin build evo_rd_platform_example
